59574a85fe62f09ed9711dd8cfaed013ae557e68
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-19 21:45:17 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
-
+from web3 import Web3
 
 class MainApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+
+    bsc = “https://bsc-dataseed.binance.org/"
+    web3 = Web3(Web3.HTTPProvider(bsc)
+    print(web3.isConnected())    
+    return MDLabel(text="Hello, World", halign="center")
 
 
 MainApp().run()
