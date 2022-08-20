d4e920e4494d1a59d0de2e53b058005a8d41e2cd
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-19 21:52:21 -1000

    First commit

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from web3 import Web3
 
 class MainApp(MDApp):
     def build(self):
-
-    bsc = “https://bsc-dataseed.binance.org/"
-    web3 = Web3(Web3.HTTPProvider(bsc)
-    print(web3.isConnected())    
-    return MDLabel(text="Hello, World", halign="center")
-
+   	bsc = “https://bsc-dataseed.binance.org/"
+   	web3 = Web3(Web3.HTTPProvider(bsc)
+   	
+	if(web3.isConnected()):    
+   		return MDLabel(text="OK connected ! ", halign="center")
+	else:
+		return MDLabel(text="OK connected ! ", halign="center")
 
 MainApp().run()
