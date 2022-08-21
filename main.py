e9681f503e1cb581e14a06e81f97716306e77814
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-20 18:02:30 -1000

    correction

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from kivymd.uix.label import MDLabel
 from web3 import Web3
 
 class MainApp(MDApp):
+    
     def build(self):
-   	bsc = “https://bsc-dataseed.binance.org/"
-   	web3 = Web3(Web3.HTTPProvider(bsc)
+        bsc = 'https://bsc-dataseed.binance.org/'
+        web3 = Web3(Web3.HTTPProvider(bsc))
    	
-	if(web3.isConnected()):    
-   		return MDLabel(text="OK connected ! ", halign="center")
-	else:
-		return MDLabel(text="KO ", halign="center")
+        if (web3.isConnected()):
+            print("Connected!")
+            return MDLabel(text="OK connected ! ", halign="center")
+        else:
+            print("KO")
+            return MDLabel(text="KO ", halign="center")
 
 MainApp().run()
