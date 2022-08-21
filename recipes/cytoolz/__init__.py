a6c78c221e6307aaf8a666928c8e58d18698bfc6
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-21 13:39:17 -1000

    Add files via upload

--- recipes/cytoolz/__init__.py
+++ recipes/cytoolz/__init__.py
@@ -1,1 +1,14 @@
+from pythonforandroid.recipe import CythonRecipe 
 
+
+class CytoolzRecipe(CythonRecipe): 
+    """Build cytoolz"""
+
+    version = "0.12.0-1"
+    url = "http://mirror.archlinuxarm.org/aarch64/community/python-cytoolz-debug-{version}-aarch64.pkg.tar.xz"
+    name = "cytoolz"
+
+    depends: List[str] = ["setuptools"]
+
+
+recipe = CytoolzRecipe()
