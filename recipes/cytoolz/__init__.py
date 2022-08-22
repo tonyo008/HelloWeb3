e26b64d3e98832e5d759f35c183bea72e3c8fe88
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-21 17:18:35 -1000

    Update __init__.py

--- recipes/cytoolz/__init__.py
+++ recipes/cytoolz/__init__.py
@@ -4,8 +4,8 @@ from pythonforandroid.recipe import CythonRecipe
 class CytoolzRecipe(CythonRecipe): 
     """Build cytoolz"""
 
-    version = "0.12.0-1"
-    url = "http://mirror.archlinuxarm.org/aarch64/community/python-cytoolz-debug-{version}-aarch64.pkg.tar.xz"
+    version = "0.12.0"
+    url = "https://github.com/pytoolz/cytoolz/archive/refs/tags/{version}.zip"
     name = "cytoolz"
 
     depends = ["setuptools"]
