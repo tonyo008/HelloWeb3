b4d29b65e6a76175f3f6c79b185d675ec198a30d
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-21 17:21:05 -1000

    Update __init__.py

--- recipes/toolz/__init__.py
+++ recipes/toolz/__init__.py
@@ -4,8 +4,8 @@ from pythonforandroid.recipe import CythonRecipe
 class ToolzRecipe(CythonRecipe): 
     """Build toolz"""
 
-    version = "0.12.0-1"
-    url = "http://mirror.archlinuxarm.org/armv7h/community/python-toolz-{version}-any.pkg.tar.xz"
+    version = "0.12.0"
+    url = "https://github.com/pytoolz/toolz/archive/refs/tags/{version}.zip"
     name = "toolz"
 
     depends = ["setuptools"]
