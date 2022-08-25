caec4eaa3bb7cc9f7c326d7eca0f2d53dd96a3cc
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-25 09:52:21 -1000

    Update __init__.py

--- recipes/setuptools/__init__.py
+++ recipes/setuptools/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class SetuptoolsRecipe(PythonRecipe):
-    version = '40.9.0'
+    version = '42.0.2'
     url = 'https://pypi.python.org/packages/source/s/setuptools/setuptools-{version}.zip'
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
