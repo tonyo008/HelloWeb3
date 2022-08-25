bd7ee3f4c3c42c741f2042194164af8edbcfe77c
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-08-25 12:08:10 -1000

    Create __init__.py

---
+++ recipes/lru-dict/__init__.py
@@ -0,0 +1,11 @@
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+
+
+class LruDictRecipe(CompiledComponentsPythonRecipe):
+    version = '1.1.5'
+    url = 'https://github.com/amitdev/lru-dict/archive/v{version}.tar.gz'
+    depends = ['setuptools']
+    call_hostpython_via_targetpython = False
+
+
+recipe = LruDictRecipe()
