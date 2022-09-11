e1576189b3c3993440d5b2723c8c7ddf0826e593
tonyo008 <111591979+tonyo008@users.noreply.github.com>
2022-09-11 08:10:36 -1000

    test plyer

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from web3 import Web3
+from plyer import notification
 
 class MainApp(MDApp):
     
     def build(self):
         bsc = 'https://bsc-dataseed.binance.org/'
         web3 = Web3(Web3.HTTPProvider(bsc))
+        
+        notification.notify(title = 'test', message= 'test message')
    	
         if (web3.isConnected()):
             print("Connected!")
